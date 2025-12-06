dcf44c429c5ec700d318a3f9312ba27232fd257d
Chunpo Wang <1505070+artoowang@users.noreply.github.com>
2025-12-06 15:25:56 -0800

    Added tool call example

--- test.sh
+++ test.sh
@@ -2,15 +2,40 @@
 
 QWEN3_8B_GGUF=~ollama/.ollama/models/blobs/sha256-a3de86cd1c132c822487ededd47a324c50491393e6565cd14bafa40d0b8e686f
 
-PROMPT="""
+PROMPT=$(cat <<EOF
 <|im_start|>system
-Speak like Yoda, in reverse.
+
+You may call one or more functions to assist with the user query.
+
+You are provided with function signatures within <tools></tools> XML tags:
+<tools>
+{'type': 'function', 'function': {
+    'description': 'Starts a new timer. If user does not specify a timer name, use the length of the timer as the name (e.g., "1 minute timer").',
+    'name': 'HassStartTimer',
+    'parameters': {
+        'properties': {
+            'hours': {'minimum': 0, 'type': 'integer'},
+            'minutes': {'minimum': 0, 'type': 'integer'},
+            'name': {'type': 'string'},
+            'seconds': {'minimum': 0, 'type': 'integer'}
+        },
+        'required': [],
+        'type': 'object',
+    },
+},
+</tools>
+
+For each function call, return a json object with function name and arguments within <tool_call></tool_call> XML tags:
+<tool_call>
+{'name': <function-name>, 'arguments': <args-json-object>}
+</tool_call>
 <|im_end|>
 <|im_start|>user
-Hello
+Timer, one minute.
 <|im_end|>
 <|im_start|>assistant
-"""
+EOF
+)
 
 # Runs model without using conversation mode. I think this is equivalent to asking the model to
 # complete the prompt?
