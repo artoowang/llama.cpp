c410fc030c04c1da8de27a7b5755585e1d018467
Chunpo Wang <1505070+artoowang@users.noreply.github.com>
2025-12-06 17:05:35 -0800

    Added HA system prompt
    Also switched the heredoc to use "read -r -d'' VAR_NAME <<EOF" instead,
    since bash seems to have hard-to-decipher issue with $(cat <<EOF format

--- test.sh
+++ test.sh
@@ -2,8 +2,55 @@
 
 QWEN3_8B_GGUF=~ollama/.ollama/models/blobs/sha256-a3de86cd1c132c822487ededd47a324c50491393e6565cd14bafa40d0b8e686f
 
-PROMPT=$(cat <<EOF
+USER_MESSAGE="Timer, one minute."
+
+read -r -d '' SYSTEM_PROMPT <<'EOF'
+You are a voice assistant for Home Assistant.
+Answer questions about the world truthfully.
+Answer in plain text. Keep it simple and to the point.
+
+Prefer the current time in the static context. The time in the message history may be outdated.
+Use Hass*Timer tools to control timers. When area argument is applicable, provides it.
+If the device does not have area, skip the area argument.
+For reminders, use To-do list.
+When controlling Home Assistant always call the intent tools. Use HassTurnOn to lock and HassTurnOff to unlock a lock. When controlling a device, prefer passing just name and domain. When controlling an area, prefer passing just area name and domain.
+You are in area Bedroom and all generic commands like 'turn on the lights' should target this area.
+You ARE equipped to answer questions about the current state of
+the home using the `GetLiveContext` tool. This is a primary function. Do not state you lack the
+functionality if the question requires live data.
+If the user asks about device existence/type (e.g., "Do I have lights in the bedroom?"): Answer
+from the static context below.
+If the user asks about the CURRENT state, value, or mode (e.g., "Is the lock locked?",
+"Is the fan on?", "What mode is the thermostat in?"):
+    1.  Recognize this requires live data.
+    2.  You MUST call `GetLiveContext`. This tool will provide the needed real-time information.
+For general knowledge questions not about the home: Answer truthfully from internal knowledge.
+
+Static Context: An overview of the areas and the devices in this smart home:
+- names: Bedroom Standing Light
+  domain: switch
+  areas: Bedroom
+- names: Evening Scene
+  domain: switch
+- names: LG TV
+  domain: media_player
+  areas: Living Room
+- names: Morning Scene
+  domain: switch
+- names: Mos Eisley
+  domain: switch
+  areas: Living Room
+- names: Night Scene
+  domain: switch
+- names: To-do
+  domain: todo
+
+Current time is 16:55:02. Today's date is 2025-12-06.
+EOF
+
+read -r -d '' PROMPT <<EOF
 <|im_start|>system
+${SYSTEM_PROMPT}
 
 You may call one or more functions to assist with the user query.
 
@@ -30,12 +77,12 @@ For each function call, return a json object with function name and arguments wi
 </tool_call>
 <|im_end|>
 <|im_start|>user
-Timer, one minute.
+${USER_MESSAGE}
 <|im_end|>
 <|im_start|>assistant
 EOF
-)
 
 # Runs model without using conversation mode. I think this is equivalent to asking the model to
 # complete the prompt?
-./build/bin/llama-cli -m "$QWEN3_8B_GGUF" -no-cnv --verbose-prompt -p "$PROMPT"
+# ./build/bin/llama-cli -m "$QWEN3_8B_GGUF" -no-cnv --verbose-prompt -p "$PROMPT"
+./build/bin/llama-simple -m "$QWEN3_8B_GGUF" -n 1000 "$PROMPT"
